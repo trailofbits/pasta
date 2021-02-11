8661fddfd9633948ec78195085414ef9fed8d6b8
Peter Goodman <peter.goodman@gmail.com>
2021-02-11 16:15:01 -0500

    Restrict the macro generator to operate only on Decl nodes

--- lib/Compile/Visitor/MacroGenerator.h
+++ lib/Compile/Visitor/MacroGenerator.h
@@ -4,52 +4,68 @@
 
 #pragma once
 
+#include <unordered_set>
+#include <string>
+
 #include <clang/AST/RecursiveASTVisitor.h>
 
 namespace pasta {
 namespace {
 std::string_view MacroAccessSpecifier(const clang::Decl *decl) {
   switch (decl->getAccess()) {
-    case clang::AccessSpecifier::AS_private: return "_PRIVATE_";
-    case clang::AccessSpecifier::AS_protected: return "_PROTECTED_";
-    default: return "_";
+    case clang::AccessSpecifier::AS_private:
+      return "_PRIVATE_";
+    case clang::AccessSpecifier::AS_protected:
+      return "_PROTECTED_";
+    default:
+      return "_";
   }
 }
-} // namespace
+}  // namespace
 
 class MacroGenerator : public clang::RecursiveASTVisitor<MacroGenerator> {
  public:
+  MacroGenerator(void) {
+#define DECL(Type, Base) acceptable_names.insert(#Type "Decl");
+#include "clang/AST/DeclNodes.inc"
+#undef DECL
+  }
+
   bool VisitCXXRecordDecl(clang::CXXRecordDecl *decl) {
     const unsigned decl_id = identifier++;
+    const auto decl_name = decl->getName().str();
+    if (!acceptable_names.count(decl_name)) {
+      return true;
+    }
 
-    llvm::errs() << "PASTA_BEGIN_CLANG_WRAPPER(" << decl->getName()
-                 << ", " << decl_id << ");\n";
+    llvm::errs() << "PASTA_BEGIN_CLANG_WRAPPER(" << decl_name << ", "
+                 << decl_id << ");\n";
 
     for (const auto method : decl->methods()) {
       llvm::errs() << "  PASTA" << MacroAccessSpecifier(method)
-                   << "METHOD(" << decl->getName()
-                   << ", " << decl_id
-                   << ", " << method->getNameInfo().getAsString() << ");\n";
+          << "METHOD(" << decl->getName() << ", " << decl_id << ", "
+          << method->getNameInfo().getAsString() << ");\n";
     }
 
     for (const auto field : decl->fields()) {
       llvm::errs() << "  PASTA" << MacroAccessSpecifier(field)
-                   << "FIELD(" << decl->getName()
-                   << ", " << decl_id
-                   << ", " << field->getName() << ");\n";
+          << "FIELD(" << decl->getName() << ", " << decl_id << ", "
+          << field->getName() << ");\n";
     }
 
-    llvm::errs() << "PASTA_END_CLANG_WRAPPER(" << decl->getName()
-                 << ", " << decl_id << ");\n\n";
+    llvm::errs() << "PASTA_END_CLANG_WRAPPER(" << decl->getName() << ", "
+                 << decl_id << ");\n\n";
 
     return true;
   }
 
  private:
-   // Unique identifier for each macro generated
-   static unsigned identifier;
+  // Unique identifier for each macro generated
+  static unsigned identifier;
+
+  std::unordered_set<std::string> acceptable_names;
 };
 
 unsigned MacroGenerator::identifier = 0;
 
-} // namespace pasta
+}  // namespace pasta

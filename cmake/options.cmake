c96113b71cad6812e1766da53922803a8f12328d
Peter Goodman <peter.goodman@gmail.com>
2021-12-17 18:11:42 -0500

    Add install targets

---
+++ cmake/options.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2020 Trail of Bits, Inc., all rights reserved.
+
+# --------------------------------------------
+# build-time options -------------------------
+# --------------------------------------------
+option(PASTA_WARNINGS_AS_ERRORS "Build treating warnings as errors" OFF)
+option(PASTA_BOOTSTRAP_MACROS "Boostrap the macro definitions" OFF)
+option(PASTA_BOOTSTRAP_TYPES "Bootstrap the type definitions from the macros" OFF)
+option(PASTA_PYTHON_BINDINGS "Build Python bindings" OFF)
+option(PASTA_ENABLE_INSTALL "Set to true to enable the install target" ON)

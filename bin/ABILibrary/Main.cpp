9a49b9b1c9383bc8b72c8f66d94a3181217adeb5
Peter Goodman <peter.goodman@gmail.com>
2022-01-04 21:28:02 -0500

    Added a mcsema abi library printer

---
+++ bin/ABILibrary/Main.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2021 Trail of Bits, Inc.
+ */
+
+#include <pasta/AST/AST.h>
+#include <pasta/AST/Decl.h>
+#include <pasta/AST/Printer.h>
+#include <pasta/AST/Token.h>
+#include <pasta/Compile/Command.h>
+#include <pasta/Compile/Compiler.h>
+#include <pasta/Compile/Job.h>
+#include <pasta/Util/ArgumentVector.h>
+#include <pasta/Util/File.h>
+#include <pasta/Util/FileSystem.h>
+#include <pasta/Util/Init.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// Visit all top-level declarations.
+class FunctionABIPrinter final : public pasta::DeclVisitor {
+ public:
+  virtual ~FunctionABIPrinter(void) = default;
+
+  void VisitDeclContext(const pasta::DeclContext &dc) {
+    for (const auto &decl : dc.AlreadyLoadedDeclarations()) {
+      Accept(decl);
+    }
+  }
+
+  void VisitTranslationUnitDecl(const pasta::TranslationUnitDecl &decl) final {
+    VisitDeclContext(decl);
+  }
+  void VisitExternCContextDecl(const pasta::ExternCContextDecl &decl) final {
+    VisitDeclContext(decl);
+  }
+
+  void VisitLinkageSpecDecl(const pasta::LinkageSpecDecl &decl) final {
+    VisitDeclContext(decl);
+  }
+
+  void VisitFunctionDecl(const pasta::FunctionDecl &decl) final {
+    std::cout << "  (void *) &" << decl.Name() << ",\n";
+  }
+};
+
+int main(int argc, char *argv[]) {
+  if (2 > argc) {
+    std::cout << "Usage: " << argv[0] << " COMPILE_COMMAND..."
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  pasta::InitPasta initializer;
+  pasta::FileManager fm(pasta::FileSystem::CreateNative());
+  auto maybe_compiler =
+      pasta::Compiler::CreateHostCompiler(fm, pasta::TargetLanguage::kCXX);
+  if (maybe_compiler.Failed()) {
+    std::cout << maybe_compiler.TakeError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto maybe_cwd = maybe_compiler->FileSystem()->CurrentWorkingDirectory();
+  if (maybe_cwd.Failed()) {
+    std::cout << maybe_compiler.TakeError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const pasta::ArgumentVector args(argc - 1, &argv[1]);
+  auto maybe_command = pasta::CompileCommand::CreateFromArguments(
+      args, maybe_cwd.TakeValue());
+  if (maybe_command.Failed()) {
+    std::cout << maybe_command.TakeError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const auto command = std::move(*maybe_command);
+  auto maybe_jobs = maybe_compiler->CreateJobsForCommand(command);
+  if (maybe_jobs.Failed()) {
+    std::cout << maybe_jobs.TakeError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  for (const auto &job : *maybe_jobs) {
+    auto maybe_ast = job.Run();
+    if (maybe_ast.Failed()) {
+      std::cout << maybe_ast.TakeError() << std::endl;
+      return EXIT_FAILURE;
+    } else {
+      FunctionABIPrinter finder;
+      for (auto tok : maybe_ast->Tokens()) {
+        std::cout << tok.Data() << ' ';
+      }
+      std::cout << "\n\nextern void *__mcsema_symbols[] = {\n";
+      finder.Accept(maybe_ast->TranslationUnit());
+      std::cout << "};\n\n";
+      return EXIT_SUCCESS;
+    }
+  }
+
+  return EXIT_FAILURE;
+}
+

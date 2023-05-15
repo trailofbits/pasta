1137859302d630cf0f33915dde4e45ed87163166
Brent Pappas <pappasbrent@gmail.com>
2023-05-15 13:27:50 -0400

    Add LowestCoveringSubstitution() method decls
    - Declare new method Stmt::LowestCoveringSubstitution() in both Stmt.h
      and GenerateStmtH.cpp.

--- bin/BootstrapTypes/GenerateStmtH.cpp
+++ bin/BootstrapTypes/GenerateStmtH.cpp
@@ -96,7 +96,8 @@ void GenerateStmtH(void) {
       os << "  friend class TokenContext;\n"
          << "  static std::optional<::pasta::Stmt> From(const TokenContext &);\n"
          << "  std::optional<::pasta::Macro> HighestContainingSubstitution(void) const noexcept;\n"
-         << "  std::optional<::pasta::MacroArgument> LowestContainingMacroArgument(void) const noexcept;\n";
+         << "  std::optional<::pasta::MacroArgument> LowestContainingMacroArgument(void) const noexcept;\n"
+         << "  std::optional<::pasta::Macro> LowestCoveringSubstitution(void) const noexcept\n;";
     }
 
     // Constructors from derived class -> base class.

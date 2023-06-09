aa1dcd49b26b24d00880b76c0fd9878e0d0d62bf
Brent Pappas <pappasbrent@gmail.com>
2023-06-09 14:37:11 -0400

    Change test layout and add predefined macro tests
    - Define all test cases at the start of the test file instead of
      scattering test case definitions throughout the file.
    - Add test cases for two predefined macros, `__STDC__`, `__FILE_NAME__`.

--- test/MacroCoveringAndContainingTests/object_like.c
+++ test/MacroCoveringAndContainingTests/object_like.c
@@ -4,6 +4,92 @@
 // RUN: print-lowest-covering-macro %s | FileCheck %s -check-prefix=PLCM
 // RUN: print-covering-macros %s | FileCheck %s -check-prefix=PCM
 
+// PHCS: 1 is contained in ONE at the highest level
+// PHCS: 'A' is contained in A at the highest level
+// PHCS: - 1 is contained in NEG_ONE at the highest level
+// PHCS: 1 is contained in NEG_ONE at the highest level
+// PHCS: 1 + 1 is contained in MID at the highest level
+// PHCS: 1 is contained in MID at the highest level
+// PHCS: 1 is contained in MID at the highest level
+// PHCS: 2 + 1 + 1 is contained in OUTER at the highest level
+// PHCS: 2 + 1 is contained in OUTER at the highest level
+// PHCS: 2 is contained in OUTER at the highest level
+// PHCS: 1 is contained in OUTER at the highest level
+// PHCS: 1 is contained in OUTER at the highest level
+// PHCS: ( 2 + ( 1 + 1 ) ) is contained in OUTER_SAFE at the highest level
+// PHCS: 2 + ( 1 + 1 ) is contained in OUTER_SAFE at the highest level
+// PHCS: 2 is contained in OUTER_SAFE at the highest level
+// PHCS: ( 1 + 1 ) is contained in OUTER_SAFE at the highest level
+// PHCS: 1 + 1 is contained in OUTER_SAFE at the highest level
+// PHCS: 1 is contained in OUTER_SAFE at the highest level
+// PHCS: 1 is contained in OUTER_SAFE at the highest level
+// PHCS: 1 is contained in ONE_PLUS_ONE at the highest level
+// PHCS: 1 is contained in ONE_PLUS_ONE at the highest level
+// PHCS: 1 is contained in ONE_PLUS at the highest level
+// PHCS: weird is contained in STRANGE at the highest level
+// PHCS: do { } while ( 0 ) is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
+// PHCS: { } is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
+// PHCS: 0 is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
+// PHCS: do { } while ( 0 ) is contained in SWALLOW_SEMICOLON at the highest level
+// PHCS: { } is contained in SWALLOW_SEMICOLON at the highest level
+// PHCS: 0 is contained in SWALLOW_SEMICOLON at the highest level
+// PHCS: 1 is contained in ONE_SEMI at the highest level
+// PHCS: return 0 is contained in EXIT_SUCCESS at the highest level
+// PHCS: 0 is contained in EXIT_SUCCESS at the highest level
+// PHCS: 1 is contained in __STDC__ at the highest level
+// PHCS: "object_like.c" is contained in __FILE_NAME__ at the highest level
+
+// PLCSD: ONE covers 1
+// PLCSD: A covers 'A'
+// PLCSD: NEG_ONE covers - 1
+// PLCSD: NOTHING covers nothing
+// PLCSD: MID covers 1 + 1
+// PLCSD: INNER covers 1
+// PLCSD: INNER covers 1
+// PLCSD: OUTER covers 2 + 1 + 1
+// PLCSD: MID covers nothing
+// PLCSD: INNER covers 1
+// PLCSD: INNER covers 1
+// PLCSD: OUTER_SAFE covers ( 2 + ( 1 + 1 ) )
+// PLCSD: MID_SAFE covers ( 1 + 1 )
+// PLCSD: INNER covers 1
+// PLCSD: INNER covers 1
+// PLCSD: ONE_PLUS_ONE covers nothing
+// PLCSD: ONE_PLUS covers nothing
+// PLCSD: DECLARE_INT_i covers int i ;
+// PLCSD: DECLARE_INT_PTR_j covers int * j ;
+// PLCSD: DECLARE_FUNC_k covers int k ( ) ;
+// PLCSD: DECLARE_FUNC_l covers int l ( ) ;
+// PLCSD: P covers nothing
+// PLCSD: INT covers nothing
+// PLCSD: STRANGE covers nothing
+// PLCSD: DO_NOT_SWALLOW_SEMICOLON covers do { } while ( 0 )
+// PLCSD: SWALLOW_SEMICOLON covers do { } while ( 0 )
+// PLCSD: ONE_SEMI covers 1
+// PLCSD: EXIT_SUCCESS covers return 0
+// PLCSD: __STDC__ covers 1
+// PLCSD: __FILE_NAME__ covers "object_like.c"
+
+// PLCM: 1 is covered by ONE at the lowest level
+// PLCM: 'A' is covered by A at the lowest level
+// PLCM: - 1 is covered by NEG_ONE at the lowest level
+// PLCM: 1 + 1 is covered by MID at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: 2 + 1 + 1 is covered by OUTER at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: ( 2 + ( 1 + 1 ) ) is covered by OUTER_SAFE at the lowest level
+// PLCM: ( 1 + 1 ) is covered by MID_SAFE at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: 1 is covered by INNER at the lowest level
+// PLCM: do { } while ( 0 ) is covered by DO_NOT_SWALLOW_SEMICOLON at the lowest level
+// PLCM: do { } while ( 0 ) is covered by SWALLOW_SEMICOLON at the lowest level
+// PLCM: 1 is covered by ONE_SEMI at the lowest level
+// PLCM: return 0 is covered by EXIT_SUCCESS at the lowest level
+// PLCM: 1 is covered by __STDC__ at the lowest level
+// PLCM: "object_like.c" is covered by __FILE_NAME__ at the lowest level
+
 // PCM: 1 is covered by ONE (kExpansion)
 // PCM: 'A' is covered by A (kExpansion)
 // PCM: - 1 is covered by NEG_ONE (kExpansion)
@@ -21,117 +107,61 @@
 // PCM: do { } while ( 0 ) is covered by SWALLOW_SEMICOLON (kExpansion)
 // PCM: 1 is covered by ONE_SEMI (kExpansion)
 // PCM: return 0 is covered by EXIT_SUCCESS (kExpansion)
+// PCM: 1 is covered by __STDC__ (kExpansion)
+// PCM: "object_like.c" is covered by __FILE_NAME__ (kExpansion)
 
 // Simple expression macros
 #define ONE 1
-// PLCSD: ONE covers 1
-// PHCS: 1 is contained in ONE at the highest level
-// PLCM: 1 is covered by ONE at the lowest level
 int one = ONE;
 
 #define A 'A'
-// PLCSD: A covers 'A'
-// PHCS: 'A' is contained in A at the highest level
-// PLCM: 'A' is covered by A at the lowest level
 char a = A;
 
 #define NEG_ONE -1
-// PLCSD: NEG_ONE covers - 1
-// PHCS: - 1 is contained in NEG_ONE
-// PHCS: 1 is contained in NEG_ONE
-// PLCM: - 1 is covered by NEG_ONE at the lowest level
 int negative_one = NEG_ONE;
 
 #define NOTHING
-// PLCSD: NOTHING covers nothing
 int nothing = NOTHING 0;
 
 // Nested object-like macros
 #define INNER 1
 #define MID INNER + INNER
 #define OUTER 2 + MID
-// PLCSD: MID covers 1 + 1
-// PLCSD: INNER covers 1
-// PLCSD: INNER covers 1
-// PLCM: 1 + 1 is covered by MID at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PLCM: 2 + 1 + 1 is covered by OUTER at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PHCS: 1 + 1 is contained in MID
-// PHCS: 1 is contained in MID 
-// PHCS: 1 is contained in MID
 int mid = MID;
-// PLCSD: OUTER covers 2 + 1 + 1
 // MID covers nothing because of operator precedence issues
-// PLCSD: MID covers nothing
-// PLCSD: INNER covers 1
-// PLCSD: INNER covers 1
-// PHCS: 2 + 1 + 1 is contained in OUTER at the highest level
-// PHCS: 2 + 1 is contained in OUTER at the highest level
-// PHCS: 2 is contained in OUTER at the highest level
-// PHCS: 1 is contained in OUTER at the highest level
-// PHCS: 1 is contained in OUTER at the highest level
 int outer_1 = OUTER;
 
 #define MID_SAFE (INNER + INNER)
 #define OUTER_SAFE (2 + MID_SAFE)
-// PLCSD: OUTER_SAFE covers ( 2 + ( 1 + 1 ) )
-// PLCSD: MID_SAFE covers ( 1 + 1 )
-// PLCSD: INNER covers 1
-// PLCSD: INNER covers 1
-// PLCM: ( 2 + ( 1 + 1 ) ) is covered by OUTER_SAFE at the lowest level
-// PLCM: ( 1 + 1 ) is covered by MID_SAFE at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PLCM: 1 is covered by INNER at the lowest level
-// PHCS: ( 2 + ( 1 + 1 ) ) is contained in OUTER_SAFE at the highest level
-// PHCS: 2 + ( 1 + 1 ) is contained in OUTER_SAFE at the highest level
-// PHCS: 2 is contained in OUTER_SAFE at the highest level
-// PHCS: ( 1 + 1 ) is contained in OUTER_SAFE at the highest level
-// PHCS: 1 + 1 is contained in OUTER_SAFE at the highest level
-// PHCS: 1 is contained in OUTER_SAFE at the highest level
-// PHCS: 1 is contained in OUTER_SAFE at the highest level
 int outer_2 = OUTER_SAFE;
 
 // Macros that cover nothing because of operator precedence issues
 #define ONE_PLUS_ONE 1 + 1
-// PLCSD: ONE_PLUS_ONE covers nothing
 
 int three = 2 * ONE_PLUS_ONE;
-// PHCS: 1 is contained in ONE_PLUS_ONE at the highest level
-// PHCS: 1 is contained in ONE_PLUS_ONE at the highest level
 #define ONE_PLUS 1 +
-// PLCSD: ONE_PLUS covers nothing
-// PHCS: 1 is contained in ONE_PLUS at the highest level
 int two = ONE_PLUS 1;
 
 // Simple declaration macros
 #define DECLARE_INT_i int i
-// PLCSD: DECLARE_INT_i covers int i
 DECLARE_INT_i;
 
 #define DECLARE_INT_PTR_j int *j
-// PLCSD: DECLARE_INT_PTR_j covers int * j
 DECLARE_INT_PTR_j;
 
 #define DECLARE_FUNC_k int k()
-// PLCSD: DECLARE_FUNC_k covers int k ( )
 DECLARE_FUNC_k;
 
 #define DECLARE_FUNC_l int l();
 
 // NOTE: Our heuristic should enable us to find this declaration
-// PLCSD: DECLARE_FUNC_l covers int l ( ) ;
 DECLARE_FUNC_l
 
 #define P p
-// PLCSD: P covers nothing
 int P = 1;
 
 /* Simple type macros*/
 #define INT int
-// PLCSD: INT covers nothing
 INT int_ = 0;
 
 /* Macros with misnested parentheses */
@@ -142,8 +172,6 @@ int weird() {
 }
 
 int foo(int argc, char const *argv[]) {
-  // PLCSD: STRANGE covers nothing
-  // PHCS: weird is contained in STRANGE at the highest level
   STRANGE);
 
 // Non-Expr Stmt macros
@@ -155,39 +183,30 @@ int foo(int argc, char const *argv[]) {
   do { \
   } while (0);
 
-  // PLCSD: DO_NOT_SWALLOW_SEMICOLON covers do { } while ( 0 )
-  // PHCS: do { } while ( 0 ) is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
-  // PHCS: { } is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
-  // PHCS: 0 is contained in DO_NOT_SWALLOW_SEMICOLON at the highest level
-  // PLCM: do { } while ( 0 ) is covered by DO_NOT_SWALLOW_SEMICOLON at the lowest level
   DO_NOT_SWALLOW_SEMICOLON;
 
   // Note: Even though the expansion includes the semicolon, our heuristics
   // should return the do-while statement, which does not include the semicolon
-  // PLCSD: SWALLOW_SEMICOLON covers do { } while ( 0 )
-  // PLCM: do { } while ( 0 ) is covered by SWALLOW_SEMICOLON at the lowest level
-  // PHCS: do { } while ( 0 ) is contained in SWALLOW_SEMICOLON at the highest level
-  // PHCS: { } is contained in SWALLOW_SEMICOLON at the highest level
-  // PHCS: 0 is contained in SWALLOW_SEMICOLON at the highest level
   SWALLOW_SEMICOLON
 
 #define ONE_SEMI 1;
 
-  // PLCSD: ONE_SEMI covers 1
-  // PHCS: 1 is contained in ONE_SEMI at the highest level
-  // PLCM: 1 is covered by ONE_SEMI at the lowest level
   ONE_SEMI
 
 #define EXIT_SUCCESS return 0;
 
-  // PLCSD: EXIT_SUCCESS covers return 0
-  // PLCM: return 0 is covered by EXIT_SUCCESS at the lowest level
-  // PHCS: return 0 is contained in EXIT_SUCCESS at the highest level
-  // PHCS: 0 is contained in EXIT_SUCCESS at the highest level
   EXIT_SUCCESS
 }
 
 int main(int argc, char const *argv[])
 {
+  // Predefined object-like macro tests NOTE(bpappas):
+
+  // We don't have automated tests for predefined outputs with dynamic output,
+  // e.g. __DATE__ and __TIME__, but they can easily be tested manually.
+  
+  __STDC__;
+  __FILE_NAME__;
+
   return 0;
 }

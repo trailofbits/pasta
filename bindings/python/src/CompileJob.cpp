c1dbec5f306bf1fd39093fe0015cfa8224b110cb
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2023-08-02 11:01:26 +0200

    Add more bindings to `CompileJob`

--- bindings/python/src/CompileJob.cpp
+++ bindings/python/src/CompileJob.cpp
@@ -8,6 +8,13 @@ namespace pasta {
 namespace nb = nanobind;
 void RegisterCompileJob(nb::module_ &m) {
   nb::class_<CompileJob>(m, "CompileJob")
+    .def_prop_ro("arguments", &CompileJob::Arguments)
+    .def_prop_ro("working_directory", &CompileJob::WorkingDirectory)
+    .def_prop_ro("resource_directory", &CompileJob::ResourceDirectory)
+    .def_prop_ro("system_root_directory", &CompileJob::SystemRootDirectory)
+    .def_prop_ro("system_root_include_directory", &CompileJob::SystemRootIncludeDirectory)
+    .def_prop_ro("target_triple", &CompileJob::TargetTriple)
+    .def_prop_ro("auxiliary_target_triple", &CompileJob::AuxiliaryTargetTriple)
     .def("run", &CompileJob::Run);
 }
 }  // namespace pasta

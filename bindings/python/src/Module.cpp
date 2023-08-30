8d3737c71e2be3709a54d4c179ca34cb72c100db
Peter Goodman <peter.goodman@gmail.com>
2023-08-30 01:25:44 -0400

    Rename case

---
+++ bindings/python/src/Module.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Trail of Bits, Inc.
+ */
+
+#include "Bindings.h"
+
+#include <pasta/Util/Init.h>
+
+namespace pasta {
+void RegisterAllAST(nanobind::module_&);
+};
+
+NB_MODULE(pypasta, m) {
+    static pasta::InitPasta initPasta;
+    pasta::RegisterArgumentVector(m);
+    pasta::RegisterCompileCommand(m);
+    pasta::RegisterFileSystem(m);
+    pasta::RegisterFileManager(m);
+    pasta::RegisterCompileJob(m);
+    pasta::RegisterCompiler(m);
+
+    pasta::RegisterAST(m);
+    pasta::RegisterAllAST(m);
+}

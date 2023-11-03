76db9f1ce16081a2b45e61adc1bc39dc761c23b5
Akshay K <iit.akshay@gmail.com>
2023-11-02 22:56:54 -0400

    update bounds to return early if decl is impicitly defaulted

--- lib/AST/Bounds.cpp
+++ lib/AST/Bounds.cpp
@@ -474,9 +474,14 @@ class DeclBoundsFinder : public clang::DeclVisitor<DeclBoundsFinder>,
       }
 
       // NOTE(pag): This is a heuristic for detecting when things go "too far."
+      // NOTE(kumarak): We have gone too far and the token bound should end
+      //                if it reaches to a namespace. Return previous token in
+      //                that case
       if (tok_kind == clang::tok::kw_namespace) {
-        assert(kind == clang::tok::kw_namespace ||
-               tok[-1].Kind() == clang::tok::kw_using);
+        if (!(kind == clang::tok::kw_namespace ||
+            tok[-1].Kind() == clang::tok::kw_using)) {
+          return &(tok[-1]);
+        }
       }
       if (tok_kind == kind && 0 >= nesting) {
         return tok;
@@ -895,7 +900,7 @@ class DeclBoundsFinder : public clang::DeclVisitor<DeclBoundsFinder>,
 
     if (proto->l_paren) {
       assert(lower_bound < proto->l_paren);
-      assert(proto->r_paren < upper_bound);
+      assert(proto->r_paren <= upper_bound);
     }
   }
 
@@ -1080,7 +1085,11 @@ class DeclBoundsFinder : public clang::DeclVisitor<DeclBoundsFinder>,
 
     clang::FunctionDecl *func =
         clang::dyn_cast<clang::FunctionDecl>(decl->getDeclContext());
-    if (!func) {
+    // Note: If the ParamVarDecl is from the implicitly defaulted FunctionDecl
+    //       then the corresponding token does not exist. No need to look for
+    //       token bounds in that case; Return early with the default initialization
+    //       of `lower_bounds` and `upper_bounds`
+    if (!func || func->isDefaulted()) {
       return;
     }
 

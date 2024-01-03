92a4c8b19651f1f3924a92e4b981843270bb50c1
Benson Liu <bensonhliu@gmail.com>
2024-01-03 09:44:49 -0800

    feat(DiffASTs): initial parsing of ASTs and setup * argument parsing with `argparse` * generating Clang ASTs using `pypasta` bindings * initial outline for producing token diffs

---
+++ bin/DiffASTs/README.md
@@ -0,0 +1,7 @@
+# Diff Clang ASTs Using Pasta + Graphtage
+
+## Usage
+```shell
+PYTHONPATH=/path/to/build/pasta/bindings/python \
+python diff.py test_before.cpp test_after.cpp
+```
---
+++ bin/DiffASTs/diff.py
@@ -0,0 +1,46 @@
+from pypasta import *
+from graphtage import printer
+from graphtage.pydiff import print_diff, build_tree
+from argparse import ArgumentParser
+import os, sys
+
+
+def _generate_ast(source_file: str, cxx: Compiler) -> AST:
+    """Generate a Clang AST from a source file."""
+    ast = None
+
+    argv = ArgumentVector(
+        [str(cxx.executable_path), "-x", "c++", "-std=c++20", source_file]
+    )
+    cmd = CompileCommand.create_from_arguments(argv, os.path.dirname(__file__))
+    jobs = cxx.create_jobs_for_command(cmd)
+
+    if isinstance(jobs, str):
+        print(jobs)
+        sys.exit(1)
+
+    for job in jobs:
+        ast = job.run()
+        if isinstance(ast, str):
+            print(ast)
+            sys.exit(1)
+
+    return ast
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="diff two Clang ASTs using pasta")
+    parser.add_argument("source_file1", type=str)
+    parser.add_argument("source_file2", type=str)
+
+    args = parser.parse_args()
+
+    fs: FileSystem = FileSystem.create_native()
+    fm: FileManager = FileManager(fs)
+    cxx: Compiler = Compiler.create_host_compiler(fm, TargetLanguage.CXX)
+
+    ast1 = _generate_ast(args.source_file1, cxx)
+    ast2 = _generate_ast(args.source_file2, cxx)
+
+    with printer.DEFAULT_PRINTER as p:
+        print_diff(ast1.main_file.tokens, ast2.main_file.tokens, printer=p)

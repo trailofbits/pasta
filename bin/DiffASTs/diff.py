96daff340b06866a797dcb70a05c6a0ca09957ac
Benson Liu <bensonhliu@gmail.com>
2024-01-08 10:08:18 -0800

    refactor(pastadiff): modularized `PastaASTBuilder`

--- bin/DiffASTs/diff.py
+++ bin/DiffASTs/diff.py
@@ -1,15 +1,12 @@
-from pypasta import *
-from graphtage import BuildOptions, ListNode, printer, StringNode, TreeNode
-from graphtage.ast import Module
-from graphtage.builder import BasicBuilder, Builder
-from graphtage.dataclasses import DataClassNode
-from graphtage.pydiff import PyDiffFormatter  # print_diff, build_tree
-from argparse import ArgumentParser
 import os, sys
-from typing import List
+from argparse import ArgumentParser
 
-from typing import Optional
-from collections.abc import Iterator
+from pypasta import *
+
+from graphtage import BuildOptions, printer
+from graphtage.pydiff import PyDiffFormatter  # print_diff, build_tree
+
+from pastadiff import PastaASTBuilder
 
 
 def _generate_ast(source_file: str, cc: Compiler, system_include_dir: str) -> AST:
@@ -41,75 +38,6 @@ def _generate_ast(source_file: str, cc: Compiler, system_include_dir: str) -> AS
             sys.exit(1)
 
     return ast
-
-
-class Function(DataClassNode):
-    name: StringNode
-    decls: ListNode
-    body: ListNode
-
-
-class Name(StringNode):
-    def __init__(self, name: str):
-        super().__init__(name, quoted=False)
-
-
-class Statement(DataClassNode):
-    kind_name: StringNode
-    decls: ListNode
-    body: ListNode
-
-
-class PastaASTBuilder(BasicBuilder):
-    @Builder.expander(TranslationUnitDecl)
-    def expand_ast(self, node: TranslationUnitDecl):
-        return (
-            decl
-            for decl in DeclContext.cast(node).declarations
-            if isinstance(decl, FunctionDecl)
-        )
-
-    @Builder.builder(TranslationUnitDecl)
-    def build_ast(self, _, children: List[TreeNode]):
-        return Module(children)
-
-    @Builder.expander(FunctionDecl)
-    def expand_function(self, node: FunctionDecl):
-        context = DeclContext.cast(node)
-        decls = context.declarations
-        children = node.body.children
-        yield from decls
-        yield from children
-
-    @Builder.builder(FunctionDecl)
-    def build_function(self, node: FunctionDecl, children: List[TreeNode]):
-        num_children = len(node.body.children)
-        num_decls = len(children) - num_children
-        decl_nodes, child_nodes = children[:num_decls], children[num_decls:]
-        return Function(name=StringNode(node.name), decls=ListNode(decl_nodes), body=ListNode(child_nodes))
-
-    @Builder.builder(Decl)
-    def build_leaf_decl(self, node: Decl, _):
-        return Name(node.name)
-
-    @Builder.expander(Stmt)
-    def expand_stmt(self, stmt: Stmt):
-        yield stmt.kind_name
-        if hasattr(stmt, "declarations"):
-            yield from stmt.declarations
-        yield from stmt.children
-
-    @Builder.builder(Stmt)
-    def build_stmt(self, stmt: Stmt, children: List[TreeNode]):
-        if hasattr(stmt, "declarations"):
-            num_declarations = len(stmt.declarations)
-        else:
-            num_declarations = 0
-        return Statement(
-            kind_name=children[0],
-            decls=ListNode(children[1:num_declarations+1]),
-            body=ListNode(children[1+num_declarations:])
-        )
 
 
 def _view_ast(ast: AST):
@@ -168,7 +96,9 @@ def main():
     ast1 = _generate_ast(args.source_file1, cc, args.system_include_dir)
     ast2 = _generate_ast(args.source_file2, cc, args.system_include_dir)
 
-    builder = PastaASTBuilder(options=BuildOptions(check_for_cycles=True))  # , ignore_cycles=True))
+    builder = PastaASTBuilder(
+        options=BuildOptions(check_for_cycles=True)
+    )  # , ignore_cycles=True))
 
     tree1 = builder.build_tree(ast1.translation_unit)
     tree2 = builder.build_tree(ast2.translation_unit)
---
+++ bin/DiffASTs/pastadiff.py
@@ -0,0 +1,81 @@
+from pypasta import *
+
+from graphtage import ListNode, StringNode, TreeNode
+from graphtage.ast import Module
+from graphtage.builder import BasicBuilder, Builder
+from graphtage.dataclasses import DataClassNode
+
+from typing import List
+
+
+class Function(DataClassNode):
+    name: StringNode
+    decls: ListNode
+    body: ListNode
+
+
+class Name(StringNode):
+    def __init__(self, name: str):
+        super().__init__(name, quoted=False)
+
+
+class Statement(DataClassNode):
+    kind_name: StringNode
+    decls: ListNode
+    body: ListNode
+
+
+class PastaASTBuilder(BasicBuilder):
+    @Builder.expander(TranslationUnitDecl)
+    def expand_ast(self, node: TranslationUnitDecl):
+        return (
+            decl
+            for decl in DeclContext.cast(node).declarations
+            if isinstance(decl, FunctionDecl)
+        )
+
+    @Builder.builder(TranslationUnitDecl)
+    def build_ast(self, _, children: List[TreeNode]):
+        return Module(children)
+
+    @Builder.expander(FunctionDecl)
+    def expand_function(self, node: FunctionDecl):
+        context = DeclContext.cast(node)
+        decls = context.declarations
+        children = node.body.children
+        yield from decls
+        yield from children
+
+    @Builder.builder(FunctionDecl)
+    def build_function(self, node: FunctionDecl, children: List[TreeNode]):
+        num_children = len(node.body.children)
+        num_decls = len(children) - num_children
+        decl_nodes, child_nodes = children[:num_decls], children[num_decls:]
+        return Function(
+            name=StringNode(node.name),
+            decls=ListNode(decl_nodes),
+            body=ListNode(child_nodes),
+        )
+
+    @Builder.builder(Decl)
+    def build_leaf_decl(self, node: Decl, _):
+        return Name(node.name)
+
+    @Builder.expander(Stmt)
+    def expand_stmt(self, stmt: Stmt):
+        yield stmt.kind_name
+        if hasattr(stmt, "declarations"):
+            yield from stmt.declarations
+        yield from stmt.children
+
+    @Builder.builder(Stmt)
+    def build_stmt(self, stmt: Stmt, children: List[TreeNode]):
+        if hasattr(stmt, "declarations"):
+            num_declarations = len(stmt.declarations)
+        else:
+            num_declarations = 0
+        return Statement(
+            kind_name=children[0],
+            decls=ListNode(children[1 : num_declarations + 1]),
+            body=ListNode(children[1 + num_declarations :]),
+        )
